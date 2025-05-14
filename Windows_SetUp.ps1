6ac724dd851372dbd6e5ee286b375637eed84a35
Filip Pašek <filip.pasek@uniprog.cz>
2025-05-14 17:06:14 +0200

    Add Links and windows setup

---
+++ Windows_SetUp.ps1
@@ -0,0 +1,18 @@
+New-Item -ItemType SymbolicLink -Path "C:/Filip/Configs/" -Target "C:/Users/Filip/AppData/Local/nvim/"
+
+# if (-not(Get-Command winget))
+# {
+# 	Install-Script -Name winget-install
+# }
+#
+# winget install Microsoft.PowerShell
+# winget install neovim
+# winget install glazewm
+# winget install git.git
+# winget install btop4win
+# winget install ditto.ditto
+# winget install eza
+# winget install brave.brave
+# winget install spotify
+# winget install discord
+#
